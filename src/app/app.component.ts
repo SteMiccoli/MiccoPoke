1b579d7167d98bd3c8f5875c5bae6a88b33f4159
SteMiccoli <stefanomiccoli03@gmail.com>
2024-04-25 16:56:47 +0200

    Working app with simple functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,71 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {
+    Component,
+    Injectable,
+    OnInit,
+    effect,
+    inject,
+    signal,
+} from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { IPokemon } from "../types";
+import { PokemonService } from "./services/pokemon.service";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: "app-root",
+    standalone: true,
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css",
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        RouterLink,
+        RouterLinkActive,
+        HeaderComponent,
+    ],
+})
+@Injectable({
+    providedIn: "root",
 })
 export class AppComponent {
-  title = 'test';
+    /**
+     * The selected pokemon
+     */
+    selectedPokemon = signal("");
+
+    /**
+     * An object containing informations about a pokemon like name, hp, defense etc.
+     */
+    pokemonData = signal<IPokemon>({} as IPokemon);
+
+    /**
+     * Variable to tell if the call function to the api has finished
+     */
+    isContentLoaded = signal(false);
+
+    private pokemonService = inject(PokemonService);
+
+    constructor() {
+        effect(() => {
+            if (this.selectedPokemon()) {
+                console.log(
+                    `The selected pokemon is: ${this.selectedPokemon()}`
+                );
+                this.pokemonService
+                    .getPokemon(this.selectedPokemon())
+                    .then((res) => {
+                        this.pokemonData.set(res.data);
+                        console.log(this.pokemonData());
+                    })
+                    .catch((err) => {
+                        this.pokemonData.set({} as IPokemon);
+                        console.error(err);
+                    })
+                    .finally(() => {
+                        this.isContentLoaded.set(true);
+                    });
+            }
+        });
+    }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { NotfoundpageComponent } from './pages/notfoundpage/notfoundpage.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { PokemonpageComponent } from './pages/pokemonpage/pokemonpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Homepage',
+        component: HomepageComponent,
+    },
+    {
+        path: ':pokemon_name',
+        title: 'PokemonPage',
+        component: PokemonpageComponent,
+    },
+    {
+        path: '**',
+        title: 'NotFoundPage',
+        component: NotfoundpageComponent,
+    },
+];
